569725249e425e7baaab739fcd6e24ecc19792a2
EljovenAleman <diegobettendorf@gmail.com>
2021-01-31 18:16:35 -0300

    Se agregaron animaciones para agacharse, pararse y saltar. Además cambia la hitbox del player al agacharse

--- JotarosBizarreQuest/Assets/Scripts/PlayerMovement.cs
+++ JotarosBizarreQuest/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    bool isWalking = false;
+    //animation parameters
+    private bool isWalking = false;
+    private bool isCrouching = false;
+    private bool isStandingUp = false;
+    private bool isJumping = false;
+
+    //HitBoxes
+    public Collider2D idleCollider;
+    public Collider2D crouchingCollider;
+    
+
+
     public float speed;
     float xMov;
     float yMov;
     Rigidbody2D rb;
     public Animator animator;
-
-
+    public float jumpForce;
+    
+    
 
     void Start()
     {
@@ -24,11 +36,46 @@ public class PlayerMovement : MonoBehaviour
         xMov = Input.GetAxisRaw("Horizontal");
         yMov = rb.velocity.y;
         rb.velocity = new Vector2(xMov * speed, yMov);
+        rb.gravityScale = 2;
+
+        if(Input.GetKeyDown(KeyCode.W) && isJumping == false)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+            animator.SetBool("Jumping", isJumping);
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            idleCollider.enabled = false;
+            crouchingCollider.enabled = true;
+            isCrouching = true;
+            isStandingUp = false;
+
+            animator.SetBool("Crouching", isCrouching);
+            animator.SetBool("StandingUp", isStandingUp);
+        }
+        else if(Input.GetKeyUp(KeyCode.S))
+        {
+            idleCollider.enabled = true;
+            crouchingCollider.enabled = false;
+            isStandingUp = true;
+            isCrouching = false;
+
+            animator.SetBool("Crouching", isCrouching);
+            animator.SetBool("StandingUp", isStandingUp);
+        }
+        
+
+
 
         if(xMov != 0)
         {
             isWalking = true;
+            isCrouching = false;
             animator.SetBool("Walking", isWalking);
+            animator.SetBool("Crouching", isCrouching);
         }
         else
         {
@@ -37,5 +84,14 @@ public class PlayerMovement : MonoBehaviour
         }
 
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Level")
+        {
+            isJumping = false;
+            animator.SetBool("Jumping", isJumping);
+        }
     }
 }
