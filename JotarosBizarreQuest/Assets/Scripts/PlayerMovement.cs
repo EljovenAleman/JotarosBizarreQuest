d8516dc090a262d6a311dfd333977a2e78db90f3
EljovenAleman <diegobettendorf@gmail.com>
2021-01-31 14:53:18 -0300

    Se crea un player, con una idle animation y walking animation

---
+++ JotarosBizarreQuest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    bool isWalking = false;
+    public float speed;
+    float xMov;
+    float yMov;
+    Rigidbody2D rb;
+    public Animator animator;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        xMov = Input.GetAxisRaw("Horizontal");
+        yMov = rb.velocity.y;
+        rb.velocity = new Vector2(xMov * speed, yMov);
+
+        if(xMov != 0)
+        {
+            isWalking = true;
+            animator.SetBool("Walking", isWalking);
+        }
+        else
+        {
+            isWalking = false;
+            animator.SetBool("Walking", isWalking);
+        }
+
+        
+    }
+}
